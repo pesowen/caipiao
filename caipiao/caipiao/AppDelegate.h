2ab0956887bf2a536657c4aa68b782c2749b69d3
pesowen <pesowen@gmail.com>
2016-03-12 20:24:09 +0800

    初始化项目

---
+++ caipiao/caipiao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  caipiao
+//
+//  Created by Owen on 16/3/12.
+//  Copyright © 2016年 Owen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
